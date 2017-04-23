7ace73ce5d66441523a6c32472ecd15dc0b018df
Davod <davidkingnt@gmail.com>
2017-04-23 00:45:12 -0300

    Update WebArchiveBOT.sh

--- bin/WebArchiveBOT.sh
+++ bin/WebArchiveBOT.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/php
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
@@ -5,15 +6,19 @@ error_reporting(E_ALL ^ E_NOTICE);
 // Edit the following as you need
 $wiki_user = '';
 $wiki_password = '';
-$wiki_url = ''; // https://commons.wikimedia.org/w/api.php
-$pages_per_query = 300; // This should be 300 for normal users, much more for bots
-$interval = 10; // Interval of every execution in minutes
-$json_file = ''; // Compressed JSON (Absolute path!)
-$json_file_cache = ''; // Uncompressed JSON for caching
-$email_operator = ''; // Your email
+$wiki_url = 'https://commons.wikimedia.org/w/api.php'; // https://commons.wikimedia.org/w/api.php
+$pages_per_query = 100; // This should be 100 for normal users, much more for bots
+$interval = 10; // Interval between every execution in minutes
+$json_file = 'archived.json.gz'; // Compressed JSON (Absolute path!)
+$json_file_cache = 'archived.json'; // Uncompressed JSON for caching (Absolute path!)
+$email_operator = "";
+
+ini_set("memory_limit",'1024M');
+
+ini_set('xdebug.var_display_max_depth',-1);
+ini_set('xdebug.var_display_max_children',-1);
+ini_set('xdebug.var_display_max_data',-1);
 
 require_once('cli.php');
-?>s
 
-
-
+?>
