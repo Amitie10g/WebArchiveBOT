e19b01535cc25fa60140ea8b8055352d90e07696
Davod <davidkingnt@gmail.com>
2017-04-23 01:06:35 -0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -3,60 +3,71 @@
 /**
  * WebArchiveBOT: botclases.php based MediaWiki for archiving external links to Web Archive
  *
- *  (c) 2015 Davod - https://commons.wikimedia.org/wiki/User:Amitie_10g
+ *  (c) 2015-2017 Davod - https://commons.wikimedia.org/wiki/User:Amitie_10g
  *
- *  This program is free software, and you are welcome to redistribute it under
- *  certain conditions. This program comes with ABSOLUTELY NO WARRANTY.
- *  see README.md and LICENSE for more information
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  **/
-	error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_ALL ^ E_NOTICE);
 
-	if(php_sapi_name() == "cli") die("\nThis script should be executed from Web.\n");
+if(php_sapi_name() == "cli") die("\nThis script should be executed from Web.\n");
 
-	$json_file = ''; // The gzipped JSON file path
-	$json_file_cache = ''; // The cached, plain JSON file path (to improve performance, specifically in Bastion server) 
-	$site_url = ''; // https://commons.wikimedia.org/wiki/
-	$sitename = ''; // Wikimedia Commons
+$json_file = 'archived.json.gz'; // The gzipped JSON file path
+$json_file_cache = 'archived.json'; // The cached, plain JSON file path (to improve performance, specifically in Bastion server) (absolute path!)
+$site_url = 'https://commons.wikimedia.org/wiki/'; // https://commons.wikimedia.org/wiki/
+$sitename = 'Wikimedia Commons'; // Wikimedia Commons
 
-	$json_contents = @file_get_contents($json_file_cache);
-	$json_contents = @json_decode($json_contents,true);
+$json_contents = json_decode(file_get_contents($json_file_cache),true);
 
 ?><html>
-	<head>
-		<title>WebArchiveBOT</title>
-		<meta charset=utf-8 />
-		<meta http-equiv="refresh" content="120" />
-		<style> 
-		body{
-			font-family:Roboto,droid-sans,Arial,sans-serif;
-		}
-		h1{
-			font-size:16pt;
-		}
-		h2{
-			font-size:14pt;
-		}
-		
-		a {
-			overflow: hidden;
-			white-space: nowrap;
-			text-overflow: ellipsis;
-		}
-	</style>
-	</head>
-		<body>
-		<div>
-			<h1>WebArchiveBOT, archived items</h1>
-			<p>This page lists the last 50 files uploaded to <?= $sitename ?> and
-			their links archived at Internet Archive by Wayback Machine. You can
-			download the <a href="<?= $json_file ?>">latest 1000 files in gziped JSON format</a>.</br>
-			For more information, see the <a href="doc" target="blank">Documentation</a>.
-			<a href="https://github.com/Amitie10g/WebArchiveBOT" target="blank">Source code</a> is available at GitHub under the GNU General Public License v3.</p>
-		</div>
-		<div>
-<?php   if(is_file($json_file)){
-                foreach($json_contents as $title=>$item){ 
+        <head>
+                <title>WebArchiveBOT, archived items</title>
+                <meta charset=utf-8 />
+                <meta http-equiv="refresh" content="120" />
+                <style>
+                        body{
+                                font-family:Roboto,droid-sans,Arial,sans-serif;
+                        }
+                        h1{
+                                font-size:16pt;
+                        }
+                        h2{
+                                font-size:14pt;
+                        }
+                        a {
+                                overflow: hidden;
+                                white-space: nowrap;
+                                text-overflow: ellipsis;
+                        }
+
+                </style>
+        </head>
+                <body>
+                <div>
+                        <h1>WebArchiveBOT, archived items</h1>
+                        <p>This page lists the last 50 files uploaded to <?= $sitename ?> and
+                        their links archived at Internet Archive by Wayback Machine. You can
+                        download the <a href="<?= $json_file ?>">latest 1000 files listed in JSON format</a>.</br>
+                        For more information, see the <a href="doc" target="blank">Documentation</a>.
+                        <a href="https://github.com/Amitie10g/WebArchiveBOT" target="blank">Source code</a> is available at GitHub under the GNU Affero General Public License v3.</p>
+                </div>
+                <div>
+<?php if(is_file($json_file_cache)){
+
+                foreach($json_contents as $title=>$item){
+
 ?>                      <h2><a href="<?= $site_url ?><?= $title ?>" target="blank"><?= $title ?></a></h2>
                         <b>Uploaded: </b><?= strftime("%F %T",$item['timestamp']) ?> (UTC)
                         <ul>
@@ -64,7 +75,7 @@
 ?>                              <li><a href="<?= $link ?>" target="blank"><?= $link ?></a></li>
 <?php                   }
 ?>
-                      </ul>
+                        </ul>
 <?php           }
 ?>
 <?php   }else{
