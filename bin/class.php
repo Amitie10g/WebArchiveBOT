1fd9f6a31d87de427d6d9725b8b0aa18a4b3343e
Davod <davidkingnt@gmail.com>
2017-04-28 02:19:09 -0300

    Update class.php

--- bin/class.php
+++ bin/class.php
@@ -404,7 +404,6 @@ class WebArchiveBOT extends Wiki {
         **/
         function getLatestFiles(){
                 $query = "?action=query&list=allimages&format=php&aisort=timestamp&aidir=older&aiprop=timestamp%7Ccanonicaltitle&ailimit=$this->pages_per_query";
-var_dump($query);
                 $query = $this->query($query);
                 return $query['query']['allimages'];
         }
