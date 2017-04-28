eaf3ecf30cd62a6abcb4a6e01427fd97b144cf46
Davod <davidkingnt@gmail.com>
2017-04-28 17:42:10 -0300

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -27,11 +27,14 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 if(php_sapi_name() == "cli") die("\nThis script should be executed from Web.\n");
 
+$site_url = 'https://commons.wikimedia.org/wiki/'; // https://commons.wikimedia.org/wiki/
+$sitename = 'Wikimedia Commons'; // Wikimedia Commons
 $json_file = 'archived.json.gz'; // The gzipped JSON file path
 $json_file_cache = 'archived.json'; // The cached, plain JSON file path (to be used by the page)
 $json_file_max_size = 1000; // Tme maximum size of the JSON file defined at the backend script
-$site_url = 'https://commons.wikimedia.org/wiki/'; // https://commons.wikimedia.org/wiki/
-$sitename = 'Wikimedia Commons'; // Wikimedia Commons
+$redis_server = '';
+$redis_port = '6379';
+$redis_id = @file_get_contents('.redis_id');
 
 define('IN_WEBARCHIVEBOT',true);
 
