0bc86910fc158c7e99a86faa2cdb06c6ea2d7606
Davod <davidkingnt@gmail.com>
2017-04-29 02:16:09 -0300

    Update WebArchiveBOT.sh

--- bin/WebArchiveBOT.sh
+++ bin/WebArchiveBOT.sh
@@ -20,143 +20,41 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  **/
 
-var_dump(realpath(dirname(__FILE__)));
+$wiki_user = '';
+$wiki_password = '';
+$wiki_url = '';
+$pages_per_query = 100;
+$interval = 10;
+$php_memory_limit = 1024;
+$public_html_path = '';
+$json_file = 'archived.json.gz';
+$json_file_cache = 'archived.json';
+$json_file_max_size = 1000;
+$email_operator = '';
+$redis_server = '';
+$redis_port = ;
 
-$config = parse_ini_file('config.ini');
+$extlinks_bl[] = '(([\p{P}\p{N}]+\.)*google\.[\p{L}\p{N}]+)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*openstreetmap\.[\p{L}\p{N}]+)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*creativecommons\.[\p{L}\p{N}]+)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*gnu\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*artlibre\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*flickr\.com)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*archive\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*facebook\.com)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wipo\.int)';
+$extlinks_bl[] = '(validator\.w3\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikipedia\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wiktionary\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikiquote\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikibooks\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikisource\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikinews\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikiversity\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikimedia\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikidata\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wmflabs\.org)';
+$extlinks_bl[] = '(([\p{L}\p{N}]+\.)*mediawiki\.org)';
 
-var_dump($config);
-
-$wiki_user = $config['wiki_user'];
-$wiki_password = $config['wiki_password'];
-$wiki_url = $config['wiki_url'];
-$pages_per_query = $config['pages_per_query'];
-$interval = $config['interval'];
-$php_memory_limit = $config['php_memory_limit'];
-$public_html_path = $config['public_html_path'];
-$json_file = $config['json_file'];
-$json_file_cache = $config['json_file_cache'];
-$json_file_max_size = $config['json_file_max_size'];
-$email_operator = $config['email_operator'];
-$redis_server = $config['redis_server'];
-$redis_port = $config['redis_port'];
-$extlinks_bl = $config['extlinks_bl'];
-
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set("memory_limit",$php_memory_limit);
-
-ini_set('xdebug.var_display_max_depth',-1);
-ini_set('xdebug.var_display_max_children',-1);
-ini_set('xdebug.var_display_max_data',-1);
-
-// Check if SAPI is CLI
-if(php_sapi_name() != "cli") die("\nThis script should be executed from CLI.\n");
-
-define('TEMP_PATH',realpath(sys_get_temp_dir()));
-
-// Set the timezone to UTC
-date_default_timezone_set('UTC');
-
-// Declare the arguments to be taken from command line. User and Password may be received
-// from the arguments --user and --password for convenience, but them can also
-// be hardcoded (see bellow)
-$shortopts  = "";
-$longopts   = array("help::","license::");
-$options	= getopt($shortopts,$longopts);
-
-$help							= $options['help'];
-$license						= $options['license'];
-
-// Declare the Help and License text
-$help_text = <<<EOH
-
-$bs::: WebArchiveBOT  Copyright (C) 2015-2017  Davod (Amitie 10g) :::$be
-
-This script is intended to check for new files uploaded to Wiki,
-extract external links and save them at Web Archive by Wayback Machine.
-$bs
-Parameters:
-
-   --user$be	 Your Wiki username (hardcoded by default)
-
-   $bs--password$be Your Wiki password (hardcoded by default)
-
-   $bs--project$be  Your Wiki projet where you  will upload your file(s),  with the
-			  "http(s)://"  prefix.  This parameter is optional;  the default
-			  value is "https://commons.wikimedia.org"
-
-   $bs--help$be	 Show this help
-
-   $bs--license$be  Show the license of this program
-
-See README.md for detailed information about its usage.
-
-EOH;
-
-$license_text = <<<EOL
-$bs
-WebArchiveBOT  Copyright (C) 2015-2017  Davod (Amitie 10g)$be
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-EOL;
-
-// Output the Help and License as requested with --help and --license
-if(isset($help)) die($help_text);
-if(isset($license)) die($license_text);
-
-require_once('class.php');
-
-$wiki = new WebArchiveBOT($wiki_url,$email_operator,$extlinks_bl,$pages_per_query,$public_html_path,$json_file,$json_file_cache,$json_file_max_size,$redis_server,$redis_port);
-
-$login = $wiki->login($wiki_user,$wiki_password);
-if($login['login']['result'] != 'Success') die('Not logged in!');
-
-$wiki->setUserAgent('WebArchiveBOT/1.0 (https://github.com/Amitie10g/WebArchiveBOT; davidkingnt@gmail.com) Botclasses.php/1.0');
-
-if(!is_int($interval)) $interval = 10;
-$interval = $interval*60;
-$result = true;
-$iteration = 0;
-while(true){
-		$time = strftime('%F %T');
-		echo "\n$time\nArchiving... ";
-
-		try{
-				if($iteration%1000 == 0) $rotate = true;
-				else $rotate = false;
-
-				$files  = $wiki->getLatestFiles();
-				$links  = $wiki->getPagesExternalLinks($files);
-				$result = $wiki->archive($links,$rotate);
-
-				if($result !== true) throw new Exception("errors ocurred when trying to archive. See the log for details.\n");
-				echo "everything OK.\n";
-				$memory_peak = memory_get_peak_usage (true);
-				echo "Memory peak: $memory_peak\n";
-
-		}catch (Exception $e){
-				$message = $e->getMessage();
-				$memory_peak = memory_get_peak_usage (true);
-
-				echo "$message\nMemory peak: $memory_peak\n";
-
-				$date = date("Y-m-d H:i:s");
-				$message .= "\n\nMemory peak: $memory_peak\n\nGenerated: $date";
-
-				$wiki->sendMail($message);
-		}
-		$iteration++;
-		sleep($interval);
-}
+require_once('cli.php')
 ?>
