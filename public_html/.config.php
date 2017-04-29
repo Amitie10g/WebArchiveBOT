b14f386a20a41ec2fc5a333a7824f5e820052f5c
Davod <davidkingnt@gmail.com>
2017-04-29 01:30:31 -0300

    Update .config.php

--- public_html/.config.php
+++ public_html/.config.php
@@ -1,12 +1,10 @@
 <?php
-
-$site_url			      = 'https://commons.wikimedia.org/wiki/'; // https://commons.wikimedia.org/wiki/
-$sitename			      = 'Wikimedia Commons'; // Wikimedia Commons
-$json_file			    = 'archived.json.gz'; // The gzipped JSON file path
-$json_file_cache  	= 'archived.json'; // The cached, plain JSON file path (to be used by the page)
+$site_url           = 'https://commons.wikimedia.org/wiki/'; // https://commons.wikimedia.org/wiki/
+$sitename           = 'Wikimedia Commons'; // Wikimedia Commons
+$json_file          = 'archived.json.gz'; // The gzipped JSON file path
+$json_file_cache    = 'archived.json'; // The cached, plain JSON file path (to be used by the page)
 $json_file_max_size = 1000; // Tme maximum size of the JSON file defined at the backend script
-$redis_server	    	= '';
-$redis_port		    	= '6379';
-$redis_id		      	= @file_get_contents('.redis_id');
-
+$redis_server       = '';
+$redis_port         = '6379';
+$redis_id           = @file_get_contents('.redis_id');
 ?>
