ef7be13fc7e880ddbd3d487e816e27c6f7710b7c
Davod <davidkingnt@gmail.com>
2017-04-29 02:16:55 -0300

    Update WebArchiveBOT.sh

--- bin/WebArchiveBOT.sh
+++ bin/WebArchiveBOT.sh
@@ -56,5 +56,5 @@ $extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wikidata\.org)';
 $extlinks_bl[] = '(([\p{L}\p{N}]+\.)*wmflabs\.org)';
 $extlinks_bl[] = '(([\p{L}\p{N}]+\.)*mediawiki\.org)';
 
-require_once('cli.php')
+require_once('cli.php');
 ?>
