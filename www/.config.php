31f9d6b25689814274b6e21d67769eecf07d8e0f
Davod <davidkingnt@gmail.com>
2018-05-25 18:40:03 -0400

    Update .config.php
    Starting moving to MySQL

--- www/.config.php
+++ www/.config.php
@@ -1,5 +1,6 @@
 <?php
 $site_url           = '';
 $sitename           = '';
+$db_type            = ''; // mysql, postgres or sqlite (default)
 $db_path            = 'webarchivebot.sqlite3'; // Absolute path!!!
 ?>
