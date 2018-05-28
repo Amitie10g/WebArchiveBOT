a625522e839eefda73658bb6508906aa3718ae59
Davod <davidkingnt@gmail.com>
2018-05-28 15:37:46 -0400

    Update .config.php

--- www/.config.php
+++ www/.config.php
@@ -1,4 +1,9 @@
 <?php
+
+//This, when running under ToolForge tool account
+$ts_pw = posix_getpwuid(posix_getuid());
+$ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
+
 $site_url           = '';
 $sitename           = '';
 $db_type            = ''; // mysql, postgres or sqlite (default)
