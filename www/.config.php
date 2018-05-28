a32a00de6cea065f0b297e46a368e321df527f3e
Davod <davidkingnt@gmail.com>
2018-05-27 21:16:24 -0400

    Update .config.php

--- www/.config.php
+++ www/.config.php
@@ -3,4 +3,8 @@ $site_url           = '';
 $sitename           = '';
 $db_type            = ''; // mysql, postgres or sqlite (default)
 $db_path            = 'webarchivebot.sqlite3'; // Absolute path!!!
+$sql_user           = '';
+$sql_password       = '';
+$sql_type           = '';
+$sql_server         = '';
 ?>
