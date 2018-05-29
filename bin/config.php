846acd7bdc8f94e6e0fa7cc6749be07c88f5c4b5
Davod <davidkingnt@gmail.com>
2018-05-29 17:25:39 -0400

    Update config.php

--- bin/config.php
+++ bin/config.php
@@ -1,7 +1,6 @@
 <?php
 
 //This, when running under ToolForge tool account
-$ts_pw = posix_getpwuid(posix_getuid());
 $ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
 
 $wiki_user = '';
