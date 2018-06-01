cefa1834d7beea1d5cbd0071bf5865ff70a2f142
Davod <davidkingnt@gmail.com>
2018-06-01 16:32:47 -0400

    Update config.php

--- www/config.php
+++ www/config.php
@@ -5,7 +5,7 @@ $ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
 
 $site_url           = '';
 $sitename           = '';
-$db_type            = ''; // mysql, postgres or sqlite (default)
+$db_type            = ''; // mysql, pgsql or sqlite (default)
 $db_server          = ''; // The DB server. For SQLite use the absolute path to the DB file
 $db_name            = ''; // For SQLite, leave empty
 $db_user            = '';
