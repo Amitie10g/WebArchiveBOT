16f486d3f012697cbe8788e332b86d6152b5d9a2
Davod <davidkingnt@gmail.com>
2018-06-02 18:52:08 -0400

    Update config.php

--- www/config.php
+++ www/config.php
@@ -3,11 +3,11 @@
 //This, when running under ToolForge tool account
 $ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
 
-$site_url           = '';
-$sitename           = '';
+$site_url           = ''; // The MediaWiki URL
+$sitename           = ''; // The wiki name
 $db_type            = ''; // mysql, pgsql or sqlite (default)
-$db_server          = ''; // The DB server. For SQLite use the absolute path to the DB file
+$db_server          = ''; // The DB server. For SQLite, use the *absolute path* to the DB file
 $db_name            = ''; // For SQLite, leave empty
-$db_user            = '';
-$db_password        = '';
+$db_user            = ''; // This too
+$db_password        = ''; // This also too
 ?>
