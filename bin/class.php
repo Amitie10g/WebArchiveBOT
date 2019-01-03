16e4e6f97febdd777d480d9db35d03624bf81ad4
Davod <davidkingnt@gmail.com>
2019-01-03 00:37:25 -0300

    Update class.php

--- bin/class.php
+++ bin/class.php
@@ -502,18 +502,17 @@ class WebArchiveBOT extends Wiki {
 		$this->db->exec("CREATE TABLE IF NOT EXISTS `data`(`id` INT NOT NULL AUTO_INCREMENT,`pageid` INT NOT NULL,`title` VARCHAR CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,`timestamp` TIMESTAMP NOT NULL,`urls` TEXT CHARACTER SET utf8 COLLATE utf8_bin,UNIQUE KEY `id` (`id`) USING BTREE,UNIQUE KEY `page_title` (`page_id`) USING BTREE,PRIMARY KEY (`id`,`page_id`)) ENGINE=InnoDB;");
 
 		foreach($pages as $page){
-
+			$title = $page['canonicaltitle'];
+			$timestamp = $page['timestamp'];
+			
 			$metadata = $this->GetPageContents($title,'externallinks');
+			$pageid = $metadata['parse']['pageid'];
+			$urls = $metadata['parse']['externallinks'];
 			
 			var_dump($metadata);
 			
-			$urls = $metadata['parse']['externallinks'];
 			if(empty($urls)) continue;
 			
-			$pageid = $metadata['parse']['pageid'];
-			
-			$title = $page['canonicaltitle'];
-			$timestamp = $page['timestamp'];
 			$urls = array_filter($urls);
 			$urls = json_encode($this->urls2archive_urls($urls));
 			
