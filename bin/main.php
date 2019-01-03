cad1cc960c9d8ead02a56aa28557db1022143063
Davod <davidkingnt@gmail.com>
2019-01-03 00:14:01 -0300

    Update main.php

--- bin/main.php
+++ bin/main.php
@@ -118,6 +118,9 @@ while(true){
 
 	try{
 		$files  = $wiki->getLatestFiles();
+		
+		var_dump($files);
+		
 		$result = $wiki->archive($files);
 
 		if($result !== true) throw new Exception("errors ocurred when trying to archive. See the log for details.\n");
