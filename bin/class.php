a425096821c2aa6835fd74de5bc01c1ae945e9e0
Davod <davidkingnt@gmail.com>
2019-01-03 22:17:26 -0300

    Update class.php

--- bin/class.php
+++ bin/class.php
@@ -508,7 +508,9 @@ class WebArchiveBOT extends Wiki {
 
 		$sql = "CREATE TABLE IF NOT EXISTS `data` (`id` INT NOT NULL AUTO_INCREMENT, `pageid` INT NOT NULL, `title` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,`timestamp` TIMESTAMP NOT NULL,`urls` TEXT CHARACTER SET utf8 COLLATE utf8_bin,UNIQUE KEY `id` (`id`) USING BTREE,UNIQUE KEY `page_title` (`page_id`) USING BTREE,PRIMARY KEY (`id`,`page_id`)) ENGINE=InnoDB;";
 		
+		echo "\n\n";
 		var_dump($sql);
+		echo "\n\n";
 		
 		$db->exec($sql);
 
