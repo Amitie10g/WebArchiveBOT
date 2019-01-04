47059158451a9c86f862a68e16b2766abb937b89
Davod <davidkingnt@gmail.com>
2019-01-03 22:11:08 -0300

    Update class.php

--- bin/class.php
+++ bin/class.php
@@ -495,7 +495,9 @@ class WebArchiveBOT extends Wiki {
 
 		try{
 			$dsn = "mysql:dbname=$this->db_name;host=$this->db_server";
-			$db = new PDO($dsn,$this->db_user,$this->db_password);	
+			$db = new PDO($dsn,$this->db_user,$this->db_password);
+			
+			var_dump($dsn);
 		}catch (PDOException $e){
    			$message = 'Connection to the DB failed';
 			echo "$message: " . $e->getMessage();
@@ -503,6 +505,7 @@ class WebArchiveBOT extends Wiki {
 			echo "\n";
 			die;
 		}
+		echo "Hola";
 
 		$sql = "CREATE TABLE IF NOT EXISTS `data`(`id` INT NOT NULL AUTO_INCREMENT, `pageid` INT NOT NULL, `title` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,`timestamp` TIMESTAMP NOT NULL,`urls` TEXT CHARACTER SET utf8 COLLATE utf8_bin,UNIQUE KEY `id` (`id`) USING BTREE,UNIQUE KEY `page_title` (`page_id`) USING BTREE,PRIMARY KEY (`id`,`page_id`)) ENGINE=InnoDB;"
 		$db->exec($sql);
