ee16bd9274886578a1f91d8b4dd38ef5ea615175
Davod <davidkingnt@gmail.com>
2019-01-03 22:46:06 -0300

    Update class.php

--- bin/class.php
+++ bin/class.php
@@ -505,7 +505,7 @@ class WebArchiveBOT extends Wiki {
 			die;
 		}
 
-		$db->exec("CREATE TABLE IF NOT EXISTS `data` (`id` INT NOT NULL AUTO_INCREMENT, `pageid` INT NOT NULL, `title` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,`timestamp` TIMESTAMP NOT NULL,`urls` TEXT NOT_NULL CHARACTER SET utf8 COLLATE utf8_bin,UNIQUE KEY `id` (`id`) USING BTREE,UNIQUE KEY `page_title` (`pageid`) USING BTREE,PRIMARY KEY (`id`)) ENGINE=InnoDB;");
+		$db->exec("CREATE TABLE IF NOT EXISTS `data` (`id` INT NOT NULL AUTO_INCREMENT, `pageid` INT NOT NULL, `title` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL, `timestamp` TIMESTAMP NOT NULL, `urls` TEXT NOT_NULL CHARACTER SET utf8 COLLATE utf8_bin, UNIQUE KEY `id` (`id`) USING BTREE, UNIQUE KEY `pageid` (`pageid`) USING BTREE, PRIMARY KEY (`id`)) ENGINE=InnoDB;");
 
 		foreach($pages as $page){
 			$title = $page['canonicaltitle'];
