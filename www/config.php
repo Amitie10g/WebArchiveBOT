92533e62ed9356b609f29824837eb569f3fa5af4
Davod <davidkingnt@gmail.com>
2019-01-04 09:51:50 -0300

    Update config.php

--- www/config.php
+++ www/config.php
@@ -3,12 +3,11 @@
 // Uncomment and set only if the 'replica.my.cnf' file cannot be set automatically (absolute path!!!)
 //$ts_mycnf         = '';
 
-$wiki_user          = ''; // Set as $ts_mycnf['user'] when using the replica file
-$wiki_password      = ''; // Set as $ts_mycnf['password'] when using the replica file
-$site_url           = ''; // The MediaWiki URL
-$sitename           = ''; // The wiki name
-$db_type            = ''; // mysql, pgsql or sqlite (default)
-$db_server          = ''; // The DB server. For SQLite, use the *absolute path* to the DB file
-$db_name            = ''; // For SQLite, leave empty
-$db_user            = ''; // This too
-$db_password        = ''; // This also too
+$wiki_user          = '';
+$wiki_password      = '';
+$site_url           = ''; // The MediaWiki API URL (eg. https://commons.wikimedia.org/w/api.php)
+$sitename           = '';
+$db_server          = '';
+$db_name            = '';
+$db_user            = ''; // Set as $ts_mycnf['user'] when using the replica file
+$db_password        = ''; // Set as $ts_mycnf['password'] when using the replica file
