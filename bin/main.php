6261f364390eefb718637590f4bae5d2208c8a9b
Davod <davidkingnt@gmail.com>
2019-01-05 20:37:34 -0300

    Update main.php

--- bin/main.php
+++ bin/main.php
@@ -87,6 +87,10 @@ EOL;
 if($help === true) die($help_text);
 if($license === true) die($license_text);
 
+var_dump($wiki_url);
+
+die;
+
 $wiki = new WebArchiveBOT($wiki_url,$email_operator,$extlinks_bl,$pages_per_query,$db_server,$db_name,$db_user,$db_password);
 
 $login = $wiki->login($wiki_user,$wiki_password);
