b22bb515e5486b4519dcb4d571574f0d1ba4d766
Davod <davidkingnt@gmail.com>
2019-01-10 11:53:18 -0300

    Update config.php

--- www/config.php
+++ www/config.php
@@ -3,12 +3,14 @@
 // Uncomment and set only if the 'replica.my.cnf' file cannot be set automatically (absolute path!!!)
 //$ts_mycnf         = '';
 
-$wiki_user          = '';
-$wiki_password      = '';
-$url                = ''; // The MediaWiki URL
-$sitename           = '';
-$db_server          = '';
-$db_name            = '';
-$db_user            = ''; // Set as $ts_mycnf['user'] when using the replica file
-$db_password        = ''; // Set as $ts_mycnf['password'] when using the replica file
-$limit              = 50;
+$wiki_user          = ''; // The Wiki username.
+$wiki_password      = ''; // The Wiki user password.
+$api_url            = ''; // The MediaWiki API URL (eg. 'https://commons.wikimedia.org/w/api.php'). 
+$wiki_url           = ''; // The MediaWiki Wiki URL (eg. 'https://commons.wikimedia.org/wiki/' with the final slash!). 
+$sitename           = ''; // The target website name (eg. 'Wikimedia Commons').
+$db_server          = ''; // The MySQL/MariaDB server address.
+$db_name            = ''; // The DB name.
+$db_user            = ''; // Set as $ts_mycnf['user'] when using the replica file (without the surrounding quotes!).
+$db_password        = ''; // Set as $ts_mycnf['password'] when using the replica file (without the surrounding quotes!).
+$db_table           = 'data'; // The table name ('data' by default)
+$limit              = 100; // Maximum pages per query (100 is a sane limit)
